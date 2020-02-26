f5e6028fcffa7a7625129f0dd7cb856c98196c4f
Jaideep <jaideep777@gmail.com>
2020-02-27 00:48:43 +0530

    fix blank screen: bind buffers before vertexAttribPointer

--- shaders/shader_fragment.glsl
+++ shaders/shader_fragment.glsl
@@ -4,8 +4,8 @@ in vec4 exCol;
 out vec4 FragColor;
 
 void main(){
-   // FragColor = exCol;
-   FragColor = vec4(1.0f, 0.5f, 0.5f, 1.0f);
+   FragColor = exCol;
+   // FragColor = vec4(1.0f, 0.5f, 0.5f, 1.0f);
 }
 
 
