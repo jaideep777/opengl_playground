71f51797c37d5973c097a86f2a42335efdd4140c
nishalsach <sachita.nishal@gmail.com>
2020-03-24 19:45:28 +0530

    24 March updates

--- shaders/shader_vertex.glsl
+++ shaders/shader_vertex.glsl
@@ -4,9 +4,11 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec4 aCol;
 out vec4 exCol;
 
+uniform float pointSize=2;
 
 void main(){
    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+   gl_PointSize = pointSize;
    exCol = aCol;
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-
+#include "../include/glinit.h"
 
 #include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
@@ -8,16 +8,16 @@
 #include <string>
 using namespace std;
 
-string errorString(GLenum err){
-	if (err == GL_INVALID_ENUM) return "GL_INVALID_ENUM";
-	else if (err == GL_INVALID_VALUE) return "GL_INVALID_VALUE";
-	else if (err == GL_INVALID_OPERATION) return "GL_INVALID_OPERATION";
-	else if (err == GL_STACK_OVERFLOW) return "GL_STACK_OVERFLOW";
-	else if (err == GL_STACK_UNDERFLOW) return "GL_STACK_UNDERFLOW";
-	else if (err == GL_OUT_OF_MEMORY) return "GL_OUT_OF_MEMORY";
-	else return "NO ERROR"; 
-//	else if (err == GL_TABLE_TOO_LARGE) return "GL_TABLE_TOO_LARGE";	
-}
+// string errorString(GLenum err){
+// 	if (err == GL_INVALID_ENUM) return "GL_INVALID_ENUM";
+// 	else if (err == GL_INVALID_VALUE) return "GL_INVALID_VALUE";
+// 	else if (err == GL_INVALID_OPERATION) return "GL_INVALID_OPERATION";
+// 	else if (err == GL_STACK_OVERFLOW) return "GL_STACK_OVERFLOW";
+// 	else if (err == GL_STACK_UNDERFLOW) return "GL_STACK_UNDERFLOW";
+// 	else if (err == GL_OUT_OF_MEMORY) return "GL_OUT_OF_MEMORY";
+// 	else return "NO ERROR"; 
+// //	else if (err == GL_TABLE_TOO_LARGE) return "GL_TABLE_TOO_LARGE";	
+// }
 
 // TODO: Should there be a GLInterface class that controls the globals? 
 //       e.g. It will store the active shapes list and active tools list, plus active window, active tool, active camera etc. 
@@ -25,56 +25,54 @@ string errorString(GLenum err){
 // 		      Maybe not?
 //            Maybe yes!
 
-void checkGLError(const char * file, int line){
-	GLenum err;
-	while((err = glGetError()) != GL_NO_ERROR){
-		cout << file << ":" << line << "   Error: " << errorString(err) << endl;
-	}
-}
+// void checkGLError(const char * file, int line){
+// 	GLenum err;
+// 	while((err = glGetError()) != GL_NO_ERROR){
+// 		cout << file << ":" << line << "   Error: " << errorString(err) << endl;
+// 	}
+// }
 
-#define CHECK_GL_ERROR() checkGLError(__FILE__, __LINE__)
-
-
-void printStatus(const char *step, GLuint context, GLuint status){
-	GLint result = GL_FALSE;
-	CHECK_GL_ERROR();
-	glGetShaderiv(context, status, &result);
-	CHECK_GL_ERROR();
-	if (result == GL_FALSE) {
-		char buffer[1024];
-		if (status == GL_COMPILE_STATUS)
-			glGetShaderInfoLog(context, 1024, NULL, buffer);
-		else
-			glGetProgramInfoLog(context, 1024, NULL, buffer);
-		if (buffer[0])
-			fprintf(stderr, "%s: %s\n", step, buffer);
-	}
-}
+// #define CHECK_GL_ERROR() checkGLError(__FILE__, __LINE__)
 
 
-GLuint loadShader(string filename, GLenum shader_type){
-	cout << "Loading shader from " << filename << endl;
-	ifstream fin(filename.c_str());
-	string c((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
-	const char * glsl_src = c.c_str();
+// void printStatus(const char *step, GLuint context, GLuint status){
+// 	GLint result = GL_FALSE;
+// 	CHECK_GL_ERROR();
+// 	glGetShaderiv(context, status, &result);
+// 	CHECK_GL_ERROR();
+// 	if (result == GL_FALSE) {
+// 		char buffer[1024];
+// 		if (status == GL_COMPILE_STATUS)
+// 			glGetShaderInfoLog(context, 1024, NULL, buffer);
+// 		else
+// 			glGetProgramInfoLog(context, 1024, NULL, buffer);
+// 		if (buffer[0])
+// 			fprintf(stderr, "%s: %s\n", step, buffer);
+// 	}
+// }
 
-	GLuint shader_id = glCreateShader(shader_type);
-	glShaderSource(shader_id, 1, &glsl_src, NULL);
-	glCompileShader(shader_id);
-	printStatus(filename.c_str(), shader_id, GL_COMPILE_STATUS);
+
+// GLuint loadShader(string filename, GLenum shader_type){
+// 	cout << "Loading shader from " << filename << endl;
+// 	ifstream fin(filename.c_str());
+// 	string c((istreambuf_iterator<char>(fin)), istreambuf_iterator<char>());
+// 	const char * glsl_src = c.c_str();
+
+// 	GLuint shader_id = glCreateShader(shader_type);
+// 	glShaderSource(shader_id, 1, &glsl_src, NULL);
+// 	glCompileShader(shader_id);
+// 	printStatus(filename.c_str(), shader_id, GL_COMPILE_STATUS);
 	
-	return shader_id;
-}
+// 	return shader_id;
+// }
 
 
 
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+// const unsigned int SCR_WIDTH = 800;
+// const unsigned int SCR_HEIGHT = 600;
 
 //const char *vertexShaderSource = "#version 330 core\n"
 //    "layout (location = 0) in vec3 aPos;\n"
@@ -91,68 +89,15 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-#endif
-
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-//    // glad: load all OpenGL function pointers
-//    // ---------------------------------------
-//    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-//    {
-//        std::cout << "Failed to initialize GLAD" << std::endl;
-//        return -1;
-//    }
-	if (gl3wInit()) {
-		printf("failed to initialize OpenGL\n");
-		return -1;
-	}
-
-
-    // build and compile our shader program
-    // ------------------------------------
-	int vertexShader = loadShader("shaders/shader_vertex.glsl", GL_VERTEX_SHADER);
-	int fragmentShader = loadShader("shaders/shader_fragment.glsl", GL_FRAGMENT_SHADER);
-	CHECK_GL_ERROR();
+    // Initialise
+    initQuickGL();
 
 //	program = glCreateProgram();
 //	glAttachShader(program, vertexShader);
 //	glAttachShader(program, fragmentShader);
 //	glLinkProgram(program);
 
-    // link shaders
-    int success;
-    char infoLog[512];
-    int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -172,30 +117,28 @@ int main()
         0, 1, 3,  // first Triangle
         1, 2, 3   // second Triangle
     };
-    unsigned int VBO, VAO, EBO, CBO; //CBO: colours buffer object
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    glGenBuffers(1, &CBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // unsigned int VBO, VAO, EBO, CBO; //CBO: colours buffer object
 
-    glBindBuffer(GL_ARRAY_BUFFER, CBO); //cbo addition
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW); //cbo addition
+    // // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    // glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    // glBindBuffer(GL_ARRAY_BUFFER, CBO); //cbo addition
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW); //cbo addition
 
-	glBindBuffer(GL_ARRAY_BUFFER, CBO);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0); // for colours
-    glEnableVertexAttribArray(1);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// glBindBuffer(GL_ARRAY_BUFFER, VBO);
+ //    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+ //    glEnableVertexAttribArray(0);
+
+	// glBindBuffer(GL_ARRAY_BUFFER, CBO);
+ //    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0); // for colours
+ //    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
@@ -228,7 +171,7 @@ int main()
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //points: GL_POINTS
         // glBindVertexArray(0); // no need to unbind it every time 
  
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -257,13 +200,6 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
-}
+
 
 
